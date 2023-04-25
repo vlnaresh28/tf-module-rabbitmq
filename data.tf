5d547cdfd7e062b511d66fe704dba831f550b489
vlnaresh28 <vlnareshreddy@gmail.com>
2023-04-25 08:27:32 +0000

    cc

--- data.tf
+++ data.tf
@@ -1,7 +1,13 @@
+//data "aws_ami" "ami" {
+//  most_recent = true
+//  name_regex  = "devops-practice-with-ansible"
+//  owners      = ["self"]
+//}
+
 data "aws_ami" "ami" {
   most_recent = true
-  name_regex  = "devops-practice-with-ansible"
-  owners      = ["self"]
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["973714476881"]
 }
 
 data "aws_route53_zone" "domain" {
--- userdata.sh
+++ userdata.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-
+labauto ansible
 ansible-pull -i localhost, -U https://github.com/vlnaresh28/roboshop-ansible roboshop.yml -e role_name=${component} -e env=${env} >/opt/ansible.log
+
+
